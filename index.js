ea5f14f2d72b75084973a01ab22b49d07d8553f0
bestzedcoder <quachlinh1252004@gmail.com>
2025-12-19 07:31:52 +0700

    ci-cd action

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 
 app.get("/", async (req, res) => {
-    res.status(200).send("Xin chào bạn");
+  res.status(200).send("Xin chào bạn 20225206");
 });
 
 // Start server
 app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+  console.log("Server running on http://localhost:3000");
 });
